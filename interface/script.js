ecc07bb98e1094f6168e9fb6734883bf578a7dbf
= <cavaleiromiau@gmail.com>
2025-05-20 21:55:28 -0300

    Fix

--- interface/script.js
+++ interface/script.js
@@ -5,3 +5,18 @@ dog.addEventListener("click", () => {
     dog.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function (){
+    nextImage();
+}, 5000)
+
+function netImage(){
+    count++;
+    if(count>4){
+        count = 1;
+}
+
+document.getElementById("radio"+count).checked = true;
